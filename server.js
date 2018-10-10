cb536210e40496de4b1714191fa35f74409d388e
Jacob Leiken <jacobleiken@gmail.com>
2018-10-09 20:45:00 -0400

    Listening on provess.env.PORT for heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/vendor', express.static(__dirname + '/vendor'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 app.use('/node_modules/axios', express.static(__dirname + '/node_modules/axios'));
  
-var server = app.listen(80, function(){
+var server = app.listen(process.env.PORT || 80, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
