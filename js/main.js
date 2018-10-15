32336b17b46765f1f69280684d3f86c28d4b37d9
Tyler Fox <tyler_fox@brown.edu>
2018-10-14 22:17:24 -0700

    :fire_engine: :sweat_drops: :fire: :100:

--- js/main.js
+++ js/main.js
@@ -6,28 +6,45 @@ let deployment = new Date(Date.UTC(2018, 8, 13, 8, 44, 6));
 
 let prevLatlng;
 
-function drawEquisatPath(latlng, map) {
-    if(prevLatlng == undefined) {
-        prevLatlng = latlng;
-    }
-    equisatPath = new google.maps.Polyline({
-        path: [prevLatlng, latlng],
-        strokeColor: "#6aa2c8",
+let curOrbitColor = "#6aa2c8";
+let nextOrbitColor = "#3b5a70";
+
+let CUR_ORBIT = 1;
+let NEXT_ORBIT = 2;
+
+var curOrbitPolyline;
+var nextOrbitPolyline;
+
+function drawOrbitPath(path, map, orbit) {
+    tempPolyline = new google.maps.Polyline({
+        path: path,
+        strokeColor: (orbit == CUR_ORBIT ? curOrbitColor : nextOrbitColor),
         strokeOpacity: 1.0,
-        strokeWeight: 2
+        strokeWeight: 3
     });
-    prevLatlng = latlng;
-    equisatPath.setMap(map);
+
+    if (orbit == CUR_ORBIT) {
+        if (curOrbitPolyline) {
+            curOrbitPolyline.setMap(null);
+        }
+        curOrbitPolyline = tempPolyline;
+    } else {
+        if (nextOrbitPolyline) {
+            nextOrbitPolyline.setMap(null);
+        }
+        nextOrbitPolyline = tempPolyline;
+    }
+    tempPolyline.setMap(map);
 };
 
 // will be called when the maps API is initialized
-function initMap() {    
+function initMap() {
 
     pvd = new google.maps.LatLng(41.826684, -71.398011);
     // create map, centered on PVD in case of failure
     map = new google.maps.Map(document.getElementById('map'), {
         center: pvd,
-        zoom: 4,   
+        zoom: 4,
         minZoom: 2,
         streetViewControl: false,
         zoomControl: false,
@@ -40,9 +57,9 @@ function initMap() {
         mapTypeControlOptions: {
               style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
               position: google.maps.ControlPosition.LEFT_BOTTOM
-        },        
+        },
         draggableCursor:'',
-        styles: [{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#19222a"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#3f4c5a"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#257bcb"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#d7dee5"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"},{"lightness":"52"},{"weight":"1"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#3f4c5a"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"},{"lightness":"14"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#3f4c5a"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#3f4c5a"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#19222a"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#19222a"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}]
+        styles: [{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"on"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#d7dee5"},{"lightness":40},{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#19222a"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#19222a"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#3f4c5a"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#257bcb"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#d7dee5"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#3f4c5a"},{"lightness":"52"},{"weight":"1"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#d7dee5"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#d7dee5"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#d7dee5"},{"lightness":"14"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#d7dee5"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#d7dee5"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#d7dee5"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#d7dee5"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#19222a"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#19222a"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"on"}]}]
     });
     // get ISS altitude and velocity
     $.get("https://api.wheretheiss.at/v1/satellites/25544", (res) => {
@@ -56,27 +73,51 @@ function initMap() {
     });
 }
 
-$(document).ready(() => {
+function getAndDrawOrbitLines() {
+    $.get("/getOrbitLines", (res) => {
+            var orbitLines = res;
+            var satCoords = [];
+            var satCoords2 = [];
+            for (var i = 0; i < orbitLines[CUR_ORBIT].length; i++) {
+              satCoords = [ ...satCoords, {lat: orbitLines[CUR_ORBIT][i][0], lng: orbitLines[CUR_ORBIT][i][1]}]; //current orbit
+            }
+            for (var i = 0; i < orbitLines[NEXT_ORBIT].length; i++) {
+              satCoords2 = [ ...satCoords2, {lat: orbitLines[NEXT_ORBIT][i][0], lng: orbitLines[NEXT_ORBIT][i][1]}]; //next orbit
+            }
+            drawOrbitLinesAfterMapInit(satCoords, map, CUR_ORBIT);
+            drawOrbitLinesAfterMapInit(satCoords2, map, NEXT_ORBIT);
+        });
+}
 
+function drawOrbitLinesAfterMapInit(satCoords, map, strokeColor) {
+    if (mapInited) {
+        drawOrbitPath(satCoords, map, strokeColor)
+    } else {
+        setTimeout(function() {drawOrbitLinesAfterMapInit(satCoords, map, strokeColor)}, 500);
+    }
+}
+
+$(document).ready(() => {
     // continually update the location of EQUiSat on the map
     setInterval(() => {
-        $.get("https://api.wheretheiss.at/v1/satellites/25544", (res) => {
-            if (!res.latitude || res.name !== "iss") {
-                console.log("something went wrong: " + JSON.stringify(res, null, 2));
-                if (!mapInited) {
-                    $("#map").html("<p>Something went wrong tracking EQUiSat. The page will reload shortly.</p>");
-                }
-            } else {
-                let lat = parseFloat(res.latitude);
-                let lng = parseFloat(res.longitude);
-                let pos = new google.maps.LatLng(lat, lng);
-                // adjust map
-                if (mapInited) {
-                    marker.setPosition(pos);
-                    map.setCenter(pos);
-                    drawEquisatPath(pos, map)
-                }
+        $.get("/getLatLon", (res) => {
+
+            let lat = parseFloat(res.lat);
+            let lng = parseFloat(res.lng);
+            let pos = new google.maps.LatLng(lat, lng);
+            // adjust map
+            if (mapInited) {
+                marker.setPosition(pos);
+                map.setCenter(pos);
+                //drawEquisatPath(pos, map)
             }
         });
     }, 1000);
+
+    getAndDrawOrbitLines();
+    setInterval(() => {
+        getAndDrawOrbitLines();
+    }, 1000*60*5);
+
+
 });
--- server.js
+++ server.js
@@ -1,16 +1,47 @@
+var fetch = require("node-fetch");
+
+const TLEJS = require('tle.js');
+const tlejs = new TLEJS();
+
+var tleStr = 'ISS (ZARYA)\n1 25544U 98067A   18167.57342809  .00001873  00000-0  35452-4 0  9993\n2 25544  51.6416  21.7698 0002962 191.5103 260.7459 15.54186563118420';
+
+function updateTLE() {
+	fetch('http://tracking.brownspace.org/api/equisat_tle').then((res) => {
+	if (res.status === 200) {
+    	res.text().then((res) => {
+        	tleStr = res;
+    	});
+    }
+});
+
+}
+
+updateTLE();
+setInterval(() => {
+	updateTLE();
+}, 1000*60*60*6);
+
 var express = require("express");
- 
+
 var app = express();
- 
+
 app.use(express.static(__dirname));
- 
+
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/vendor', express.static(__dirname + '/vendor'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 app.use('/node_modules/axios', express.static(__dirname + '/node_modules/axios'));
- 
+
+app.get('/getLatLon', function (req, res) {
+  res.send(tlejs.getLatLon(tleStr));
+})
+
+app.get('/getOrbitLines', function (req, res) {
+  res.send(tlejs.getGroundTrackLatLng(tleStr));
+})
+
 var server = app.listen(process.env.PORT || 80, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
